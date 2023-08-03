959c04b8e7c352e299c2cba5e7139474df3654a9
EC2 Default User <ec2-user@ip-172-31-10-127.ap-northeast-1.compute.internal>
2023-08-03 13:41:44 +0000

    [Update]genre_paramsの追加、topページ変数修正

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -5,6 +5,7 @@ class Admin::GenresController < ApplicationController
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
 
   def create
@@ -13,5 +14,12 @@ class Admin::GenresController < ApplicationController
   end
 
   def update
+    @genre = Genre.find(params[:id])
   end
+end
+
+private
+
+def genre_params
+  params.require(:genre).permit(:name)
 end
