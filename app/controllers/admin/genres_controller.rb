4ce4114459a954027f90b45d5855dd1e9e63630d
EC2 Default User <ec2-user@ip-172-31-10-127.ap-northeast-1.compute.internal>
2023-08-05 09:26:26 +0000

    [Update]genresコントローラーupdateアクション追記、genreのeditページ作成、indexページ修正

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -15,6 +15,8 @@ class Admin::GenresController < ApplicationController
 
   def update
     @genre = Genre.find(params[:id])
+    @genre.update(genre_params)
+    redirect_to admin_genres_path(@genre.id)
   end
 end
 
