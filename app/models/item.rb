a9ffcca02081789efecf61d915596acf008e9d0d
EC2 Default User <ec2-user@ip-172-31-10-127.ap-northeast-1.compute.internal>
2023-08-09 12:10:34 +0000

    [Update/Add]商品詳細ページ作成、商品一覧ページに画像が表示されるようエラー解消、cart_itemモデルにvalidates設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,11 +12,11 @@ class Item < ApplicationRecord
   validates :is_sold, presence: true
 
   def get_item_image(width, height)
-  #unless item_image.attached?
-    #file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
-    #profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-  #end
-   item_image.variant(resize_to_limit: [width, height]).processed
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_limit: [width, height]).processed
   end
 
   def price_with_tax
