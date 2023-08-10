7e549c51825bac6e370244c2fca48fb492d80415
EC2 Default User <ec2-user@ip-172-31-10-127.ap-northeast-1.compute.internal>
2023-08-10 11:32:48 +0000

    [Update/Add]items/editタイトル背景記述修正、customers_controllerにストロングパラメータ記述、会員情報編集画面作成

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -14,6 +14,13 @@ class Admin::CustomersController < ApplicationController
   def update
     @customer = Customer.find(params[:id])
     @customer.update(customer_params)
-    redirect_to customer_path(@customer.id)
+    redirect_to admin_customer_path(@customer.id)
   end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:email, :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :is_deleted)
+  end
+
 end
