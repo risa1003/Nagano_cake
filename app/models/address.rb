c6c0fc80030a1419110c3ec574d4d934c667b8fa
EC2 Default User <ec2-user@ip-172-31-10-127.ap-northeast-1.compute.internal>
2023-08-19 12:49:36 +0000

    [Update]admin/orders/showページのtfoot追加、customers/edit・items/index・items/showタイトル修正、public/orders/new作成

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,8 @@ class Address < ApplicationRecord
   belongs_to :customer
 
   validates :postal_code, :address, :receiver_name, presence: true
+
+  def address_display
+    '〒' + postal_code + ' ' + address + ' ' + receiver_name
+  end
 end
