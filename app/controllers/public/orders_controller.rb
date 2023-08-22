5a78b264b1b7c99973cbff90ee1e917ef1abc6a8
EC2 Default User <ec2-user@ip-172-31-10-127.ap-northeast-1.compute.internal>
2023-08-22 12:05:59 +0000

    [Update]orders_controllerのparamater・select_addressの記述変更、thanksページ作成、confirm/viewのお届け先記述をaddress_displayと同様に変更

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,8 +4,25 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm #要確認
-    @order = Order.new
-    @order.save
+    @order = Order.new(order_params)
+    if params[:order][:select_address] == "1"
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.receiver_name = current_customer.last_name + current_customer.first_name
+    elsif params[:order][:select_address] == "2"
+      ship = Address.find(params[:order][:address_id])
+      @order.postal_code = ship.postal_code
+      @order.address = ship.address
+      @order.receiver_name = ship.receiver_name
+    #elsif params[:order][:select_address] == "3"
+    #   @order.postal_code = params[:order][:postal_code]
+    #   @order.address = params[:order][:address]
+    #   @order.receiver_name = params[:order][:receiver_name]
+    # else
+    #   render 'new'
+    end
+    @cart_items = current_customer.cart_items.all
+    @total_amount = @cart_items.sum(&:subtotal)
   end
 
   def thanks
@@ -25,5 +42,11 @@ class Public::OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:payment_type, :address, :postal_code, :receiver_name)
   end
 end
