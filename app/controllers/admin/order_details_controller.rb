f9c331b5e93f7747603e219811867c58b89f9c03
EC2 Default User <ec2-user@ip-172-31-10-127.ap-northeast-1.compute.internal>
2023-09-14 12:16:18 +0000

    [Update]製作ステータス更新時に注文ステータスも更新されるようにエラーを修正

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,19 +1,13 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
-    @order_detail = Order.find(params[:id])
-    @order_detail.update(order_detail_params)
-    redirect_to order_detail_path
-  end
-
-  def update
-    @order = Order.find(params[:order_id])
+    @order = Order.find(params[:order_detail][:order_id])
     @order_detail = OrderDetail.find(params[:id])
     @order_details = @order.order_details.all
 
     is_updated = true
     if @order_detail.update(order_detail_params)
       # 製作ステータスが「製作中」のときに、注文ステータスを「製作中」に更新する。
-      @order.update(status: 2) if @order_detail.in_production == "in_progress"
+      @order.update(status: 2) if @order_detail.product_status == "in_production"
       # ここから下の内容は③の内容になります。
       # 紐付いている注文商品の製作ステータスが "すべて" [製作完了]になった際に注文ステータスを「発送準備中」に更新する。
       @order_details.each do |order_detail|
@@ -25,5 +19,10 @@ class Admin::OrderDetailsController < ApplicationController
       @order.update(status: 3) if is_updated
     end
     redirect_to admin_order_path(@order)
+  end
+
+  private
+  def order_detail_params
+    params.require(:order_detail).permit(:id, :product_status)
   end
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -9,13 +9,13 @@ class Admin::OrdersController < ApplicationController
     @order_details = OrderDetail.where(order_id: params[:id])
     if @order.update(order_params)
       #注文ステータスが「入金確認」の時、製作ステータスを全て「製作待ち」に更新
-      @order_details.update_all(status: 1) if @order.status == "payment_confirmed"
+      @order_details.update_all(product_status: 1) if @order.status == "payment_confirmed"
     end
+    redirect_to admin_order_path(@order)
   end
 
   private
   def order_params
     params.require(:order).permit(:id, :status)
-    params.require(:order_detail).permit(:id, :product_status)
   end
 end
